2b7b4d423a331e2ff01de8de987d27039d56ae39
Tiago <tiagobnarita@gmail.com>
2023-09-29 12:31:28 -0300

    Creating Post Request

---
+++ src/main/java/med/voll/API/medico/DadosCadastroMedico.java
@@ -0,0 +1,6 @@
+package med.voll.API.medico;
+
+import med.voll.API.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+}
